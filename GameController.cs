5c7080b538f292103168d9d46d30d613e8b699a7
alu0100909678 <alu0100909678@ull.edu.es>
2018-10-31 11:34:44 +0000

    Añadiendo scripts y GIF's

---
+++ GameController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+    public delegate void PlayerEvents();
+    public static event PlayerEvents CambioLuz;
+    public static event PlayerEvents AumentoPoder;
+    public static event PlayerEvents ReduccionPoder;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.H))
+        {
+            CambioLuz();
+        }            
+    }
+
+    public static void AumentarPoder()
+    {
+        if (AumentoPoder != null)
+        {
+            AumentoPoder();
+        }
+    }
+
+    public static void ReducirPoder()
+    {
+        if (ReduccionPoder != null)
+        {
+            ReduccionPoder();
+        }
+    }
+}
---
+++ ObjectA.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectA : MonoBehaviour {
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        GameController.AumentarPoder();
+    }
+
+	public void cambiarTam () {
+		transform.localScale += new Vector3(0.5F, 0.5F, 0.5F);
+	}
+}
